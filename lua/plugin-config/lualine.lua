14ad104ce7095ef7fef7f66520c99c322c6a404f
shaojiale <shaojiale@bytedance.com>
2023-03-13 16:52:46 +0800

    update: show coc stat in middle.

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -171,27 +171,32 @@ ins_left {
 }
 
 ins_left {
-    -- Lsp server name .
-    function()
-        --local msg = 'No Active Lsp'
-        local msg = 'sjl\'s fuckin cool editor'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then
-            return msg
-        end
-        for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
-            end
-        end
-        return msg
-    end,
+    'g:coc_status',
     icon = ' :',
     color = { fg = '#a9a1e1', gui = 'bold' },
 }
 
+--[[ins_left {]]
+--[[-- Lsp server name .]]
+--[[function()]]
+--[[--local msg = 'No Active Lsp']]
+--[[local msg = 'sjl\'s fuckin cool editor']]
+--[[local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')]]
+--[[local clients = vim.lsp.get_active_clients()]]
+--[[if next(clients) == nil then]]
+--[[return msg]]
+--[[end]]
+--[[for _, client in ipairs(clients) do]]
+--[[local filetypes = client.config.filetypes]]
+--[[if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then]]
+--[[return client.name]]
+--[[end]]
+--[[end]]
+--[[return msg]]
+--[[end,]]
+--[[icon = ' :',]]
+--[[color = { fg = '#a9a1e1', gui = 'bold' },]]
+--[[}]]
 -- Add components to right sections
 
 ins_right { 'location' }
