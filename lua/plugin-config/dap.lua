b3c79c46dfc2571346b8af152958d5e1ba1568a9
shaojiale <shaojiale.gitignore@icloud.com>
2023-04-04 22:40:38 +0800

    update: use 'dw' insteand of 'dn'.

--- lua/plugin-config/dap.lua
+++ lua/plugin-config/dap.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>dt', function() require('dap').run_to_cursor() end)
 vim.keymap.set('n', '<leader>dr', function() require('dap').clear_breakpoints() end)
 vim.keymap.set('n', '<leader>ds', function() require('dap').step_into() end)
 vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end)
-vim.keymap.set('n', '<leader>dn', function() require('dap').step_over() end)
+vim.keymap.set('n', '<leader>dw', function() require('dap').step_over() end)
 vim.keymap.set('n', '<leader>da', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('v', '<leader>de', function() require('dapui').eval() end)
 
