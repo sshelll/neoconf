fc9f6287c20f3ccd9f77f9b93308efda1aa543af
shaojiale <shaojiale.gitignore@icloud.com>
2023-04-15 00:47:23 +0800

    feat: add rust debugger.

--- lua/plugin-config/dap.lua
+++ lua/plugin-config/dap.lua
@@ -8,6 +8,15 @@ dap.adapters.delve = {
     }
 }
 
+dap.adapters.codelldb = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = '/usr/local/bin/codelldb/adapter/codelldb',
+    args = {'--port', '${port}'},
+  }
+}
+
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
     {
@@ -25,3 +34,20 @@ dap.configurations.go = {
         program = "./${relativeFileDirname}"
     }
 }
+
+local dap = require('dap')
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
