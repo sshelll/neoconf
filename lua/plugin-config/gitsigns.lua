500ead82efeabeb9a17fe2e30f73193df2822065
kayce <kayce@bituniverse.org>
2023-06-26 10:46:41 +0800

    update: add easymotion shortcut && configured gitsigns.

--- lua/plugin-config/gitsigns.lua
+++ lua/plugin-config/gitsigns.lua
@@ -8,14 +8,14 @@ require('gitsigns').setup {
         untracked    = { text = '┆' },
     },
     signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+    numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
     linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir                 = {
         follow_files = true
     },
     attach_to_untracked          = true,
-    current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts      = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
