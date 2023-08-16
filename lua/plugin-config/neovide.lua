cdfd6b92a58d33735333c193976a581ebfb11646
kayce <kayce@bituniverse.org>
2023-08-16 19:36:34 +0800

    update: neovide

--- lua/plugin-config/neovide.lua
+++ lua/plugin-config/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
-    vim.g.guifont = 'Monaco:h12:#e-subpixelantialias:#h-none'
     vim.g.neovide_cursor_vfx_mode = 'railgun'
     vim.g.neovide_hide_mouse_when_typing = true
+    vim.o.guifont = "JetBrainsMono Nerd Font:h14"
     vim.g.neovide_fullscreen = true
 end
