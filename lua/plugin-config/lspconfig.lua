fc517b724f8958cc16437fdb44a30bc4dd41dd30
sshelll <shaojiale.gitignore@icloud.com>
2023-09-29 16:56:15 +0800

    feat: add customized snippets.

--- lua/plugin-config/lspconfig.lua
+++ lua/plugin-config/lspconfig.lua
@@ -36,8 +36,49 @@ lspconfig.lua_ls.setup {
     },
 }
 
+local kind_icons = {
+    Text = "",
+    Method = "󰆧",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "󰇽",
+    Variable = "󰂡",
+    Class = "󰠱",
+    Interface = "",
+    Module = "",
+    Property = "󰜢",
+    Unit = "",
+    Value = "󰎠",
+    Enum = "",
+    Keyword = "󰌋",
+    Snippet = "",
+    Color = "󰏘",
+    File = "󰈙",
+    Reference = "",
+    Folder = "󰉋",
+    EnumMember = "",
+    Constant = "󰏿",
+    Struct = "",
+    Event = "",
+    Operator = "󰆕",
+    TypeParameter = "󰅲",
+}
+
 -- Setup nvim-cmp
 cmp.setup {
+    formatting = {
+        format = function(entry, vim_item)
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+                path = "[Path]",
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+            })[entry.source.name]
+            return vim_item
+        end
+    },
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
@@ -72,11 +113,11 @@ cmp.setup {
         end, { 'i', 's' }),
     }),
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer' },
-        { name = 'nvim_lua' },
+        { name = 'nvim_lsp', priority = 1000 },
+        { name = 'luasnip',  priority = 900 },
+        { name = 'nvim_lua', priority = 800 },
+        { name = 'path',     priority = 800 },
+        { name = 'buffer',   priority = 500 },
     },
 }
 
@@ -178,3 +219,5 @@ end
 -- show diagnostic info in float window
 vim.o.updatetime = 250
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.fn.stdpath "config" .. "/snippets" }
