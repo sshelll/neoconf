01c5a973b6b666a2bc91414bbf1ae2b5caf6ab30
sshelll <shaojiale.gitignore@icloud.com>
2023-10-10 19:35:30 +0800

    update: treesitter.

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -1,26 +1,34 @@
-require'nvim-treesitter.configs'.setup {
-  -- 安装 language parser
-  -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = {"html", "css", "vim", "lua", "javascript", "typescript", "tsx", "go"},
-  -- 启用代码高亮功能
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  -- 启用增量选择
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      node_incremental = '<CR>',
-      node_decremental = '<BS>',
-      scope_incremental = '<TAB>',
-    }
-  },
-  -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
-  --indent = {
+require 'nvim-treesitter.configs'.setup {
+    -- 安装 language parser
+    -- :TSInstallInfo 命令查看支持的语言
+    ensure_installed = { "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "go" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    -- 启用代码高亮功能
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    },
+    -- 启用增量选择
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<CR>',
+            node_incremental = '<CR>',
+            node_decremental = '<BS>',
+            scope_incremental = '<TAB>',
+        }
+    },
+    -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
+    --indent = {
     --enable = true
-  --}
+    --}
 }
 
 -- 开启 Folding
