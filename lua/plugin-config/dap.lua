6e01065434d2eca65d1db48d0796dc330b86d0f1
sshelll <shaojiale.gitignore@icloud.com>
2023-10-19 16:46:00 +0800

    fix: rust dap.

--- lua/plugin-config/dap.lua
+++ lua/plugin-config/dap.lua
@@ -84,5 +84,5 @@ dap.configurations.cpp = {
 }
 
 dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+-- dap.configurations.rust = dap.configurations.cpp
 dap.configurations.typescript = dap.configurations.javascript
