38f663b851772254e65240cc437a76afab1c8560
kayce <kayce@bituniverse.org>
2023-11-15 11:40:47 +0800

    update: precheck binary installed.

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,17 +1,37 @@
+local jdtls_bin = vim.fn.stdpath('data') .. '/mason/bin/jdtls'
+local java_debug_jar = vim.fn.glob(vim.fn.stdpath('data') ..
+    '/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1)
+local vscode_java_test_jars = vim.fn.glob(vim.fn.stdpath('data') ..
+    '/vscode-java-test/server/*.jar', 1)
+
+if vim.fn.filereadable(jdtls_bin) == 0 then
+    print('jdtls binary not found')
+    return
+end
+
+if java_debug_jar == '' then
+    print('java debug jar not found')
+    return
+end
+
+if vscode_java_test_jars == '' then
+    print('vscode java test jars not found')
+    return
+end
+
 local config = {
-    cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
+    cmd = { jdtls_bin },
     root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
 
 local bundles = {
-    vim.fn.glob(vim.fn.stdpath('data') ..
-        '/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1),
+    java_debug_jar,
 };
 
-vim.list_extend(bundles, vim.split(vim.fn.glob(vim.fn.stdpath('data') ..
-    '/vscode-java-test/server/*.jar', 1), '\n'))
+vim.list_extend(bundles, vim.split(vscode_java_test_jars, '\n'))
 config['init_options'] = {
     bundles = bundles,
 }
 
 require('jdtls').start_or_attach(config)
+require('jdtls').setup_dap()
