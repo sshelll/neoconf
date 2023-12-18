231caa1d6c3e51aacf3ec3f0abcb89837ddc626e
kayce <kayce@bituniverse.org>
2023-12-18 17:14:48 +0800

    feat(nvim-surround): add surround plug.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,13 @@
 local pluglist = {
     {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup()
+        end
+    },
+    {
         "b0o/incline.nvim",
         lazy = false,
         config = function()
