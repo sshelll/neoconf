a76aa4039a14700c93cb4184a7cf52cd0138e603
kayce <kayce@bituniverse.org>
2023-12-19 14:35:57 +0800

    feat(other.nvim): add a plug for switch relative files.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,13 @@
 local pluglist = {
     {
+        "rgroli/other.nvim",
+        lazy = false,
+        event = "VeryLazy",
+        config = function()
+            require('plugin-config/other')
+        end
+    },
+    {
         "kylechui/nvim-surround",
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
