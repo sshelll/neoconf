3a846844225aa27945876f150efdaedff12a2be1
kayce <kayce@bituniverse.org>
2023-12-28 16:39:12 +0800

    feat(neovide): conf changed.

--- lua/plugin-config/neovide.lua
+++ lua/plugin-config/neovide.lua
@@ -1,6 +1,11 @@
 if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = 'railgun'
     vim.g.neovide_hide_mouse_when_typing = true
-    vim.o.guifont = "JetBrainsMono Nerd Font:h14"
+    vim.o.guifont = "MesloLGS Nerd Font:h12"
     vim.g.neovide_fullscreen = true
+    vim.opt.linespace = 0
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
 end
