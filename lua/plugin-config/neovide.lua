c613455fb834f7a6d9738a837e93032555c6e97e
kayce <kayce@bituniverse.org>
2024-01-02 14:12:30 +0800

    feat(neovide): change animation time.

--- lua/plugin-config/neovide.lua
+++ lua/plugin-config/neovide.lua
@@ -8,6 +8,7 @@ if vim.g.neovide then
     vim.g.neovide_padding_bottom = 0
     vim.g.neovide_padding_right = 0
     vim.g.neovide_padding_left = 0
+    vim.g.neovide_scroll_animation_length = 0.1
     vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
     vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
     vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
