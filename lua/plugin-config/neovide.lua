5d33614e85e1d0d59bad0c76b93f4c9c852ac130
kayce <kayce@bituniverse.org>
2024-04-15 17:25:36 +0800

    feat(neovide): change font.

--- lua/plugin-config/neovide.lua
+++ lua/plugin-config/neovide.lua
@@ -1,9 +1,11 @@
 if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = 'railgun'
     vim.g.neovide_hide_mouse_when_typing = true
-    vim.o.guifont = "MesloLGS Nerd Font:h12:#h-normal"
+    -- vim.o.guifont = "MesloLGS Nerd Font:h12:#h-full"
+    vim.o.guifont = "JetBrainsMono Nerd Font:h12:#h-full"
     vim.g.neovide_fullscreen = true
     vim.opt.linespace = 0
+    vim.g.neovide_scale_factor = 1.0
     vim.g.neovide_padding_top = 0
     vim.g.neovide_padding_bottom = 0
     vim.g.neovide_padding_right = 0
