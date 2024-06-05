2a317e02ba855af47fef3a90b98cb5e6122fdc99
kayce <kayce@bituniverse.org>
2024-06-05 10:36:53 +0800

    fix(bash): add prefix dot for run cmd.

--- lua/util/bash.lua
+++ lua/util/bash.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.ui_run = function()
     local file = vim.fn.expand('%:p')
-    require("util/cmd").term_exec_cmd(file)
+    require("util/cmd").term_exec_cmd("." .. file)
 end
 
 return M
